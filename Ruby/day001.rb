441180d7e6a29726b306ac9f80bc03d52590d718
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-18 16:32:33 +0300

    Adding day001.rb

---
+++ Ruby/day001.rb
@@ -0,0 +1,17 @@
+# Basic Printing To The Screen
+
+puts "Hello World!"
+puts "Did you know that Software Engineering is one point in software development?"
+
+puts "The 9 Software Development Core Activities Are:"
+
+print "Processes, "
+print "Requirements, "
+print "Design, "
+print "Engineering, "
+print "Construction, "
+print "Testing, "
+print "Debugging, "
+print "Deployment, "
+print "and "
+print "Maintenance "
