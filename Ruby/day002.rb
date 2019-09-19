fdd2e769a4b31363bff6596a62ee0a01628cc8bc
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-19 14:11:12 +0300

    Adding day002.rb

---
+++ Ruby/day002.rb
@@ -0,0 +1,17 @@
+# Basic String Methods
+
+
+puts "hello!".upcase
+puts "The first hello was in lowercase before applying the method to manipulate it into an upper case."
+
+puts "HELLO!!".downcase
+puts "The second hello, was in uppercase before applying the method to manipulate it into a lower case."
+
+puts "hello!!!".capitalize
+puts "The third hello, was in all lower case letters before applying the capitalize method to manipulate it into a capitalized string."
+
+puts "hello!!!!".reverse
+puts "This fourth hello was a hello before using the reverse method to reverse it into.. . this thing!"
+
+puts "hello!!!!!".length
+puts "In this fifth hello I've used a method to not manipulate the string, but to get the length of the string."
