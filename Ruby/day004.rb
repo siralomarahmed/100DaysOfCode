0c7f465d1b96a875de3fcdf50e53274bbd116555
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-21 14:16:44 +0300

    Adding day004.rb

---
+++ Ruby/day004.rb
@@ -0,0 +1,16 @@
+# Basics of Getting User Input
+
+
+print "Please enter your name: "
+name = gets
+
+print "Please enter you age: "
+age = gets
+
+print "Please enter your hobbies: "
+hobbies = gets
+
+
+puts name
+puts age
+puts hobbies
