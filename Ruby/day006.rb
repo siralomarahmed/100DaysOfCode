6c53321b5af4555f34fd9ee13002f3fe13a2c541
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-23 09:32:57 +0300

    Adding day006.rb

---
+++ Ruby/day006.rb
@@ -0,0 +1,16 @@
+# Basic Math Using Ruby
+
+puts "_Basic math operation with integers:"
+
+puts "Addition: 1 + 2 = #{1 + 2}"
+puts "Subtraction: 3 - 4 = #{3 - 4}"
+puts "Multiplication: 5 * 6 = #{5 * 6}"
+puts "Division: 5 / 2 = #{7 / 8}"
+puts "Modulus: 9 % 10 = #{9 % 10}"
+puts "Exponent: 2 ** 4 = #{2 ** 4}"
+
+
+puts "_Division with and without Floats"
+
+puts "Using Integers as an input: 10 / 3 = #{10 / 3}"
+puts "Using Floats as an input:  10.0 / 3.0 = #{10.0 / 3.0}"
