041fc2bc620297896e59f2162bb100776d19a6d4
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-29 12:12:13 +0300

    Adding day012.rb

---
+++ Ruby/day012.rb
@@ -0,0 +1,37 @@
+# Basic Arrays in Ruby
+
+employees = [
+  "Ahmed",
+  "Stella",
+  "Britton",
+  "Riley",
+  "Dana"
+]
+puts employees
+
+
+hobbies = [
+  "Reading",
+  "Walking",
+  "Swimming",
+  "Sports"
+]
+puts hobbies
+
+
+numbers = [
+  1,
+  11,
+  111,
+  1111,
+  11111
+]
+puts numbers
+
+
+mix_array = [
+  1,
+  "Names",
+  hobbies,
+]
+puts mix_array
