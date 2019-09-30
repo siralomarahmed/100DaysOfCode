a823da0c52964dcd610288bac69dd4bf7420717b
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-30 13:27:42 +0300

    Adding day013.rb

---
+++ Ruby/day013.rb
@@ -0,0 +1,15 @@
+# Basic Arrays Interactions in Ruby
+
+animal_list = [
+  "Bird",
+  "Dog",
+  "Cat",
+  "Turtle",
+  "Shark"
+]
+
+puts "where did you go, youu #{animal_list[2]}?"
+
+puts "The length of the animal list is: #{animal_list.length}"
+
+puts "The last item in the anima list is a: #{animal_list[animal_list.length - 1]}"
