5d9b93e6a58730298f2016cd9779a28338f538aa
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-03 10:36:18 +0300

    Adding day016.rb

---
+++ Ruby/day016.rb
@@ -0,0 +1,18 @@
+# Basic Loops in Ruby - Each Loop
+
+# Example 1
+(1..5).each do |num|
+  puts num
+end
+
+
+# Example 2
+players = [
+"John",
+"Dan",
+"Bob"
+]
+
+players.each do |name|
+  puts name
+end
