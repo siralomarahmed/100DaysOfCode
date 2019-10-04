60ffde71df4b03bb0f0a344606ca10d06e76d096
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-04 10:14:04 +0300

    Adding day017.rb

---
+++ Ruby/day017.rb
@@ -0,0 +1,12 @@
+# Hashes in Ruby
+
+favorite_pizza = {
+  "Ahmed" => "Neapolitan Margherita",
+  "Max" => "Chicago deep dish",
+  "Rin" => "Greek Vigiterian",
+  "May" => "Detroit pepperoni"
+}
+
+puts favorite_pizza["Max"]
+
+puts "Did you know that May like #{favorite_pizza["May"]} pizza?"
