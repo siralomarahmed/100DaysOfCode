6204b227bd0977abbfa4609486cc4dd416d3b778
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-05 10:17:43 +0300

    Adding day018.rb

---
+++ Ruby/day018.rb
@@ -0,0 +1,15 @@
+# Ruby Methods!
+
+# Example 1
+def hello()
+  puts "Hello World."
+end
+
+hello
+
+# Example 2
+def greet(name)
+  puts "Hello #{name}, welcome to the party!"
+end
+
+greet("Ahmed")
