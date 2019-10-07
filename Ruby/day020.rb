edf307e4dc1351d3f7d28b46e7ccc04f7fb27f08
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-07 08:34:48 +0300

    Adding day020.rb

---
+++ Ruby/day020.rb
@@ -0,0 +1,22 @@
+# Basics of Classes in Ruby - part 1
+
+class First_class
+  # do something.. .
+end
+
+class Second_class
+  # Describing the class behavior
+end
+
+class Third_class
+  # Simply a bunch of methods here!
+end
+
+
+=begin
+  First_class
+  Second_class
+  Third_class
+
+  Are all global constant now.
+=end
