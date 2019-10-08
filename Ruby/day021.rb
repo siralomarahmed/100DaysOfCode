237b0493d5302d15abded041028be3c05e33bac7
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-08 09:04:24 +0300

    Adding day021.rb

---
+++ Ruby/day021.rb
@@ -0,0 +1,24 @@
+# Basics of Classes in Ruby - part 2
+
+class First_class
+
+  def initilize()
+  end
+
+end
+
+class Second_class
+
+  def initilize(x)
+    # Do something with x
+  end
+
+end
+
+class Third_class
+
+  def initilize()
+    print "Hello Class!"
+  end
+
+end
