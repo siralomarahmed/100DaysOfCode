b917623112c8b4a9f336e4c3fc2d7061c7610056
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-17 14:27:44 +0300

    Adding day030.rb

---
+++ Ruby/day030.rb
@@ -0,0 +1,34 @@
+# Intermediate practice on printing in Ruby
+
+# Using puts to have a new line or space in the output
+puts "In 
+Computer Science... ."
+
+puts
+puts
+puts
+puts "Data Structure:"
+
+
+# Using print to write a paragraph..
+print "Is a combination of"
+print " "
+print "data organization,"
+print " "
+print "data managment,"
+print " "
+print "and"
+print " "
+print "storage format."
+
+# make use of puts to separate the output hahaha...
+puts
+puts
+
+
+p "Ahmed Alomar"
+
+
+# Noitice the new line and how it will be represented in the output
+p "Data structures 
+ are generally based on the ability of a computer to fetch and store data at any place in its memory"
