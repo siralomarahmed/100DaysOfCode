5963fc85766c32311900d599e0ae7f53747b5475
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-18 14:29:52 +0300

    Adding day031.rb

---
+++ Ruby/day031.rb
@@ -0,0 +1,33 @@
+# deep dive into Variables
+
+# Variable Syntax
+
+name = "Ahmed"
+last_name = "Alomar"
+handsome = true
+age = 29 + 1
+
+
+puts name
+puts last_name
+puts
+
+puts name + " " + last_name
+puts
+
+puts handsome
+puts
+
+puts age
+puts
+
+age = 50
+puts age
+puts
+
+favorite_number = 24
+p favorite_number
+puts
+
+favorite_number = "Twenty-four"
+p favorite_number
