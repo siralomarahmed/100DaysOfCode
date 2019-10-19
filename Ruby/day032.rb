9b4d102e25054b318eed8a64c89a4b26f54ffeb1
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-19 15:51:39 +0300

    Adding day032.rb

---
+++ Ruby/day032.rb
@@ -0,0 +1,20 @@
+# Intermediate Variable - Parallel Variable
+
+a = 10
+b = 20
+c = 30
+
+# Instead of writing everything on a separate line like this example here:
+p a
+p b
+p c
+
+puts
+
+# You can write all the variables in one line, comma seperated.
+p a, b, c
+
+puts
+
+# You can use print so they are all in the same line when you output them
+print a, b, c
