9be8a611191fc27463c05071a972933413e1c548
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-21 14:33:41 +0300

    Adding day034.rb

---
+++ Ruby/day034.rb
@@ -0,0 +1,24 @@
+# Ruby Constant
+
+# A changing variable Example:
+age = 30
+p age
+
+age = 40
+p age
+
+
+# A constant variable Example:
+NAME = "Ahmed"
+AGE = 30
+
+p NAME
+p AGE
+
+p "My name is " + NAME
+
+
+# Ruby allow change but is is not good!
+NAME = "Mr.Awesome"
+
+p "Now I am " + NAME
