9987e65170584ac3137df4341e4c9a5ec8fa5ea7
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-24 12:04:32 +0300

    Adding day037.rb

---
+++ Ruby/day037.rb
@@ -0,0 +1,14 @@
+# Input and output program
+
+
+# To get input from the user about the name
+puts " Hi, What's your name?"
+name = gets.chomp
+
+# To get input from the user about the age
+puts "Great! What's your age?"
+age = gets.chomp
+
+
+# To output a string response
+puts "Cool, so your name is #{name} and you are #{age} years old."
