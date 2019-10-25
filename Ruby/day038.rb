8f00957048133b270e716a37a2701e42fbe8fa58
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-25 10:55:44 +0300

    Adding day038.rb

---
+++ Ruby/day038.rb
@@ -0,0 +1,25 @@
+# Intermediate Numbers and the ".class" Method
+
+# Integers numbers
+p 5
+p -3
+p 0
+p -200
+
+# Float numbers
+p 5.34
+p -9.653
+p 11.23456
+
+puts
+
+# Using the .next method
+p 9.next
+p 19.next
+
+puts
+
+# Using the .class method
+p 4.class
+p 7.3.class
+p 9999999999999999999999999999999999999999999999.class
