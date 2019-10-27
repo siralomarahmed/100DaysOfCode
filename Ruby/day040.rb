281bbe458b47c3b8a8d2e610e53fbb97852bff95
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-27 12:06:05 +0300

    Adding day040.rb

---
+++ Ruby/day040.rb
@@ -0,0 +1,23 @@
+# Intermediate Booleans in Ruby
+
+handsome = true
+stupid = false
+
+p handsome
+p stupid
+
+puts
+#  Check things out with the class method
+p handsome.class
+p stupid.class
+
+puts
+# .odd? .even? Methods
+
+p 1.next
+p 1.to_s
+
+p 10.odd?
+p 11.odd?
+p 12.even?
+p 13.even?
