2645c4a3f387ed5312252656d4874afc5705d6bf
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-28 11:14:07 +0300

    Addign day041.rb

---
+++ Ruby/day041.rb
@@ -0,0 +1,26 @@
+# Basic Artiemitic Methods
+
+p 1.next()
+
+puts
+# Arthemetic Methods:
+p 1.+(9)
+p 2.-(8)
+p 3.*(7)
+p 4.**(6)
+p 5./(5)
+p 5.%(5)
+
+puts
+# Methods with the same functionality
+# Division example
+p 10 / 5
+p 10./(5)
+p 10.div(5)
+
+
+puts
+# Modulo example
+p 10 % 3
+p 10.%(3)
+p 10.modulo(3)
