5a322ebf18e3371ed4032599a661744a1be97c46
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-10-30 11:57:16 +0300

    Adding day043.rb

---
+++ Ruby/day043.rb
@@ -0,0 +1,18 @@
+# Basic Float Methods in Ruby
+
+p 10.9.to_i
+p 10.9.to_i.class
+
+############
+puts
+
+p 10.5.floor
+p 10.5.ceil
+
+#############
+puts
+p 3.14259.round(4)
+
+#############
+puts
+p 34.67.abs
