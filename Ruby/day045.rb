179c0e6327ce79f9359e536095b702b7aec63737
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-01 11:58:28 +0300

    Adding day045.rb

---
+++ Ruby/day045.rb
@@ -0,0 +1,7 @@
+# Basic of Blocks in Ruby - part 2
+
+3.times do |x|
+  puts "We are currently on loop number #{x +1}"
+  puts "Ahmed is incredible."
+  puts "I'm having so much fun learning Ruby."
+end
