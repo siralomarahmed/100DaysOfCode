05c38bebd2a222f6f967c08da6b31bb2f9c650dc
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-02 12:22:40 +0300

    Adding day046.rb

---
+++ Ruby/day046.rb
@@ -0,0 +1,19 @@
+# More object methods with Ruby blocks
+
+
+5.downto(0) do |current_number|
+  puts "We are currently on #current_number}"
+  puts "Horray!"
+end
+puts "LIFTOFF!"
+
+
+####
+puts
+####
+
+
+5.upto(10) do |x|
+  puts "We are moving on up!"
+  puts "We are currently on #{x}"
+end
