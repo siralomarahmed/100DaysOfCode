f1d040a18b14eb253e784ef27ec17a8ba38564bd
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-05 11:43:19 +0300

    Adding day049.rb

---
+++ Ruby/day049.rb
@@ -0,0 +1,18 @@
+# Strings Escape characters and Qoutes!
+
+# Ignore example
+puts "Juliet said \"Goodbye\" to Romeo"
+
+# New line example
+puts "Let's add a line break\nright here"
+
+puts "\tInsert a tab right here."
+
+
+# Qoutes example
+puts "Hello\nWorld"
+puts 'Hello\nWorld'
+
+phrase = String.new("Awesome Day!")
+puts "#{phrase}"
+puts '#{phrase}'
