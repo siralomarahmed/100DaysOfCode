96cff63abe66111da1ffc2989556c87e4ae17a17
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-08 14:57:20 +0300

    Adding day052.rb

---
+++ Ruby/day052.rb
@@ -0,0 +1,18 @@
+# Extract a character from string.
+
+story = "Once upon a time in a land far, far away"
+
+# To find the length
+p story.length
+
+# Example of calling an index number in range of the string length.
+p story[2]
+
+# Example to call an index less than zero.
+p story[-1]
+
+# Example of calling an index that is more than the length of the string
+p story[100]
+
+# Example of using the .slice method
+p story.slice(5)
