fbdf3f122989e402b5e651d45c9ec0f08ccacf82
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-10 16:36:10 +0300

    Adding day 054.rb

---
+++ Ruby/day054.rb
@@ -0,0 +1,15 @@
+# Takeing characters from string using the range method
+
+coding_theory = "Coding theory is the study of the properties of codes and their fitness for a specific application"
+
+p coding_theory[17..26]
+
+puts
+
+p coding_theory[34..39]
+p coding_theory[34...39]
+p coding_theory.slice(87..-1)
+
+
+puts
+p coding_theory.slice(48..200)
