8eb80b32ae7337444d319106a8baf0621e5d6017
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-11 16:46:02 +0300

    Adding day055.rb

---
+++ Ruby/day055.rb
@@ -0,0 +1,24 @@
+# Changing character of a string
+
+
+# Changing one character
+word = "It was nice"
+p word
+word[7] = ""
+p word
+
+puts
+# Changing two character
+some_tale = "This horse tail was very long"
+p some_tale
+some_tale[13] = "l"
+some_tale[14] = "e"
+p some_tale
+
+puts
+# another one with multicharacters
+
+sweet = "I love Strawberry pie"
+p sweet
+sweet[7, 5] = "Blue"
+p sweet
