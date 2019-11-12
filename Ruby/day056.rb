55065534e778938bc3740ec8e06a9723d86a0600
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-12 16:39:49 +0300

    Adding day056.rb

---
+++ Ruby/day056.rb
@@ -0,0 +1,29 @@
+# More Common String Methods
+
+puts "computation science".capitalize
+puts "computation science".capitalize.class
+
+puts
+
+puts "PracTiCal".downcase
+puts "PracTiCal".downcase.class
+
+puts
+
+puts "Theoretical Science".upcase
+puts "Theoretical Science".upcase.class
+
+puts
+
+puts "Ahmed Alomar".swapcase
+puts "Ahmed Alomar".swapcase.class
+
+puts 
+
+puts "Applications".reverse
+puts "Applications".reverse.class
+
+puts
+
+puts "Ruby Is Fun".upcase.reverse.downcase
+puts "Ruby Is Fun".upcase.reverse.downcase.class
