383759e50879a7f36753866b1135a8eed706c0c9
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-11-13 16:32:52 +0300

    Adding day057.rb

---
+++ Ruby/day057.rb
@@ -0,0 +1,19 @@
+# The Bang Method in Ruby
+
+word = "programming"
+p word
+p word.upcase
+p word
+
+puts
+
+complex = "system"
+p complex
+p complex = complex.upcase
+p complex
+
+puts
+
+p digital = "Gottfried Leibniz"
+digital.upcase!
+p digital
