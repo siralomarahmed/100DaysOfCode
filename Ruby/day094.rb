7caacd12ee4eda07002c428535a3dfed5afbc736
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-12-20 12:42:24 +0300

    Adding day094.rb

---
+++ Ruby/day094.rb
@@ -0,0 +1,20 @@
+# Importing Modules
+
+require "./resources/rectangle.rb"
+require "./resources/square.rb"
+require "./resources/circle.rb"
+
+puts Circle.area(5)
+puts Rectangle.area(10, 10)
+puts Square.area(15)
+
+
+puts
+
+# The Math Module
+puts Math::PI
+
+puts Math.sqrt(16)
+puts Math.sqrt(20.23)
+
+puts Math.sin(90)
