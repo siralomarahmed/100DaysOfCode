6b8b50eb6e9031f9acef1441c994900122d5245f
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-02 13:59:21 +0300

    Adding day002.c

---
+++ C/day002.c
@@ -0,0 +1,32 @@
+/*
+*
+* Written by Ahmed A. M. Alomar
+*
+*/
+
+
+/********************************
+ * Make a comment stand out
+ * Like an important thing here.
+ * ******************************/
+
+
+/*
+This program display multiple strings.
+a demonstration of the Escape sequence is provided in between the strings too.
+*/
+
+
+#include <stdio.h> // This is a preprocessor that include the library of stdio.h
+
+// THis is the main function of the program
+int main()
+{
+  // This is an output statements
+    printf("This is written in C.\\ This language is very interesting\b");
+    printf("The world is very\t beautiful. isn\'t it\?\n");
+    printf("He said, \"Her smile is charming\". Then he blushed.");
+
+    // This is a return statement
+    return 0;
+}
