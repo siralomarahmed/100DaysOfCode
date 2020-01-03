656bdf4556f49f8ff665ea499b1dda3734a4d056
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-03 13:59:11 +0300

    Adding day003.c

---
+++ C/day003.c
@@ -0,0 +1,37 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 3 - 2020
+Description:
+This is where the Documentation is written, at the top of the program
+
+        (Documentation Section)
+
+The idea of having a structure of a program is aimed at improving the clarity, quality, and development time of a computer program. **************/
+
+
+/**************
+Preprocessor Statements are libraries that will be linked in the program.
+
+        (Link Section)
+
+An example of preprocessors is the #include <stdio.h>	heading library.
+*/
+
+
+/**************
+
+Global Declarations place is used to declare all variables that will be used within the program. More like on the Global Scope if defined here outside any function.
+
+        (Definition Section)
+
+This is the place to declare constants too.
+*/
+
+
+/*************
+It is the home of functions. Every program function is going to be at this part of the file.
+
+        (Functionalities)
+
+The main() function is an example of what can go on here.
+Any other function can be written after the main function.
+*/
