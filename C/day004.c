cfd6ba2b0b36fad5e6898dae3ba9c280236c3d26
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-04 12:19:33 +0300

    Adding day004.c

---
+++ C/day004.c
@@ -0,0 +1,15 @@
+/* 
+The include preprocessor
+Adding the stdio header file
+*/
+
+#include <stdio.h>
+
+
+
+// The main program function starts here
+int main()
+{
+  printf("Preprocessors are very cool");
+  return 0;
+}
