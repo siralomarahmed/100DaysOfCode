9b0b8a31ca964a4f295fe09f669323d77e6ae2c3
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-05 12:10:19 +0300

    Adding day005.c

---
+++ C/day005.c
@@ -0,0 +1,26 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 5 - 2020
+Description:
+Variables declaration examples */
+
+
+int main()
+{
+    // variable deceleration
+    int house;
+
+    // Multiple decleration on one line
+    int house_1, house_2, house_3;
+
+    // Spaghetti naming
+    int myfirstvariable;
+
+    // Snake case naming
+    int my_second_variable;
+
+    // capitalized naming
+    int myThirdVariable;
+
+    // Camel case naming
+    int MyFourthVariable;
+}
