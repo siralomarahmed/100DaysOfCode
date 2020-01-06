812e1ba330f8d639bc1b8b38c4c4a51cad8458cd
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-06 12:51:02 +0300

    Adding day006.c

---
+++ C/day006.c
@@ -0,0 +1,20 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 6 - 2020
+Description:
+Variables Initialization examples */
+
+
+int main()
+{
+    // Separate deceleration
+    int box_1;
+
+    // Separate Initialization
+    box_1 = 10;
+
+    // Deceleration and Initialization on the same line
+    int box_2 = 20;
+
+    // Multiple deceleration and Initialization on the same line
+    int box_3 = 30, box_4 = 40, box_5 = 50;
+}
