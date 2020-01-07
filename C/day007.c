e0725e04fc47d2523a78dfd877395cc1bd909ebc
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-07 14:05:20 +0300

    Adding day007.c

---
+++ C/day007.c
@@ -0,0 +1,17 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 7 - 2020
+Description:
+Format Specifier examples.
+Format specifiers are flags that represent the variable in statements. */
+
+
+#include <stdio.h>
+
+int main()
+{
+    int box = 10;
+
+    printf("The value of the box is: %d", box);
+    
+    return 0;
+}
