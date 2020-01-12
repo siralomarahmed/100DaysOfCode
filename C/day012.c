ac01abc8105f5ff04218a3d230939aec6f44c712
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-12 07:03:24 +0300

    Adding day012.c

---
+++ C/day012.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 12 - 2020
+Description:
+This program is an example for a float data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  float box_1 = 123.456;
+  float box_2 = -50.80;
+
+
+  printf("%f\n", box_1);
+  printf("%f\n", box_2);
+
+  return 0;
+}
