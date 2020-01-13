e6645377cb57da5b2c090c468d436a13cb54da2e
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-13 12:04:54 +0300

    Adding day013.c

---
+++ C/day013.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 13 - 2020
+Description:
+Tlis program is an example for a double data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  double box_1 = 741.2589;
+  double box_2 = -96743.852;
+
+
+  printf("%lf\n", box_1);
+  printf("%lf\n", box_2);
+
+  return 0;
+}
