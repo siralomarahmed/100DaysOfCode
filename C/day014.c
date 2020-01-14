bb5205d2188d448dd881a2693db60acbf6c7f33c
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-14 12:34:23 +0300

    Adding day014

---
+++ C/day014.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 14 - 2020
+Description:
+This program is an example for a long double data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  long double box_1 = 785296852341.2532165489;
+  long double box_2 = -967425825828523.84452;
+
+
+  printf("%Lf\n", box_1);
+  printf("%Lf\n", box_2);
+
+  return 0;
+}
