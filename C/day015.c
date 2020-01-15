a7f4ed42a15ae472f1d94ba8e1437d848891273c
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-15 12:06:22 +0300

    Adding day015.c

---
+++ C/day015.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 15 - 2020
+Description:
+This program is an example for a boolean data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  _Bool box_1 = 0;
+  _Bool box_2 = 1;
+
+
+  printf("%i\n", box_1);
+  printf("%d\n", box_2);
+
+  return 0;
+}
