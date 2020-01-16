51f2d4cf604008e8442d1d4ac4556273967824a0
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-16 12:08:45 +0300

    Adding day016.c

---
+++ C/day016.c
@@ -0,0 +1,25 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 16 - 2020
+Description:
+This program is an example for the character data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  // declaring a char type data
+  char myCharacter;
+
+  // initializing the char type data
+  myCharacter = 'A';
+
+  // declaring and initializing a character with the ACCII code
+  char wonderWhat = 50;
+
+  // print out
+  printf("%c \n", myCharacter);
+  printf("%c \n", wonderWhat);
+
+  return 0;
+}
