a12a3a48244f72e60d477f1e1e61efaa380e87c5
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-17 10:59:17 +0300

    Adding day017.c

---
+++ C/day017.c
@@ -0,0 +1,17 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 17 - 2020
+Description:
+This program is an example for the octal data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  int box1  = 15;
+
+  // Octal using a format specifier
+  printf("The content of the box is: %o\n", box1);
+
+  return 0;
+}
