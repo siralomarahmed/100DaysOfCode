715a23d6eb9ec35a66179933fde50bad92f33408
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-18 11:30:20 +0300

    Adding day018.c

---
+++ C/day018.c
@@ -0,0 +1,17 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 18 - 2020
+Description:
+This program is an example for the hexadecimal data type */
+
+
+#include <stdio.h>
+
+int main()
+{
+  int box1 = 15;
+
+  // Hexadecimal using a format specifier
+  printf("The content of the box is: %x\n", box1);
+
+  return 0;
+}
