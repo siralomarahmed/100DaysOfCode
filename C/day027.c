b1c75ca8fcc1fe0862d224e3ba0356110f343e76
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2020-01-27 14:56:43 +0300

    Adding day027.md

---
+++ C/day027.c
@@ -0,0 +1,27 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: January 27 - 2020
+Description:
+This program is an example for a basic control flow */
+
+
+#include <stdio.h>
+
+// Function Call Statement
+int main()
+{
+  // Variable deceleration statements
+  int x;
+  int box;
+
+  // Variable initiation statement
+  box = 25;
+
+  // Condition statement with a logical operator
+  x = box >= 13 && box <= 19;
+
+  // Print statement
+  printf("x = %d \n", x);
+
+  // return statement
+  return 0;
+}
