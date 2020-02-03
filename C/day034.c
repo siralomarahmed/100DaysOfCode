e46efef8ee03b8f9398766a67cab8a0f9105b73b
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-03 16:32:59 +0300

    Adding day034.c

---
+++ C/day034.c
@@ -0,0 +1,18 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 3 - 2020
+Description:
+This program is an example of a For Loop Statement.
+This Program will print a hello 5 times. */
+
+
+#include <stdio.h>
+
+int main()
+{
+
+  for (int x = 1; x <= 5 ; ++x) {
+    printf("Hello\n");
+  }
+
+  return 0;
+}
