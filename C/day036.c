4a7fd6112dba84164e2a645814f58e7fb0fd417c
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-05 17:55:56 +0300

    Adding day036.c

---
+++ C/day036.c
@@ -0,0 +1,21 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 5 - 2020
+Description:
+This program is an example of a While loop Statement.
+This Program will print hello 5 times. */
+
+
+#include <stdio.h>
+
+int main()
+{
+  int x = 0;
+
+  do {
+    printf("Hello\n");
+    x++;
+  }
+  while (x <5);
+
+  return 0;
+}
