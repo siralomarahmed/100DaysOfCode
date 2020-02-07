d7c445e364bc8f41e5482aa6579c85f9e770e8e9
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-07 15:57:48 +0300

    Adding day038.c

---
+++ C/day038.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 7 - 2020
+Description:
+This program is an example of a Arrays - Declaration
+This code shows the difference between declaring variables and arrays */
+
+
+int main()
+{
+  // Declaring 5 variables separately:
+  int box_1;
+  int box_2;
+  int box_3;
+  int box_4;
+  int box_5;
+
+  // Declaring an array size 5 that hold 5 integer values
+  float arrayBox[5];
+}
