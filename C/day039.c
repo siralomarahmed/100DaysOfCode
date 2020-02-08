aef9a4b2c16947cde8f4c31199f0e9f35bab1f6b
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-08 17:30:04 +0300

    Adding day039.c

---
+++ C/day039.c
@@ -0,0 +1,12 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 8 - 2020
+Description:
+This program is an example of a Arrays Initialization */
+
+
+int main()
+{
+  // Declaring & Initializing an array
+  int box[10]= {10, 20, 30, 40};
+
+}
