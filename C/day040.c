bf74d373bf4bca0fc98f7e366f0e210b777b4970
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-09 14:05:44 +0300

    Adding day040.c

---
+++ C/day040.c
@@ -0,0 +1,20 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 9 - 2020
+Description:
+This program is an example of a Array designated initialization */
+
+
+int main()
+{
+  int myArray[10] = {
+    [3] = 1200,
+    [6] = 2400,
+    [9] = 3500,
+    [2] = 2000,
+    [4] = 4000,
+    [8] = 8000,
+    [1] = 500,
+    [0] = 10,
+    [5] = 50
+  };
+}
