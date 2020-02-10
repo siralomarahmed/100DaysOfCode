5123021c660a967f00b8e8016fc913d48e10fc75
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-10 10:57:36 +0300

    Adding day041.c

---
+++ C/day041.c
@@ -0,0 +1,16 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 10 - 2020
+Description:
+This program is an example of a Array designated assignment */
+
+
+int main()
+{
+  int myArray[10];
+
+  myArray[2] = 100;
+
+  myArray[5] = 500;
+
+  myArray[8] = 800;
+}
