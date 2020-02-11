10db796c162bcbf5d10bdb01aeed2da26f4547b2
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-11 11:41:58 +0300

    Adding day042.c

---
+++ C/day042.c
@@ -0,0 +1,15 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 11 - 2020
+Description:
+This program is an example of Multidimensional Array */
+
+
+int main()
+{
+  int myArray[4][3] = {
+    {10, 11, 12},
+    {20, 21, 22},
+    {30, 31, 32},
+    {40, 41, 42}
+  };
+}
