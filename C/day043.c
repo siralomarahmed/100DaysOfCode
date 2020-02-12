6d1c8102852db6e84036802ff49355408328f20b
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-12 11:27:30 +0300

    Adding day043.c

---
+++ C/day043.c
@@ -0,0 +1,16 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 12 - 2020
+Description:
+This program is an example of a how to access an array element and print out the value stored in the element  */
+
+
+#include <stdio.h>
+
+int main()
+{
+  int box[5] = {10, 20, 30, 40};
+
+  printf("%d", box[2]);
+
+  return 0;
+}
