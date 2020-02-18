fac6628cf3fac5a530d696a1228570dc85325fb5
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-18 10:09:48 +0300

    Adding day049.c

---
+++ C/day049.c
@@ -0,0 +1,20 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 18 - 2020
+Description:
+This program is an example of a custom function definition*/
+
+
+#include <stdio.h>
+
+// Custom function
+void myFunction()
+{
+  printf("Hello Functions");
+}
+
+
+// The main function
+int main()
+{
+  return 0;
+}
