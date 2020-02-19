a1557d3c24c4d542f9ddbc95d4466279dd09048f
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-19 09:59:49 +0300

    Adding day050.c

---
+++ C/day050.c
@@ -0,0 +1,30 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 19 - 2020
+Description:
+This program is an example of a function Decleration */
+
+
+#include <stdio.h>
+
+// function prototypes = decleration
+void x();
+void y();
+
+
+// the main function
+int main()
+{
+  return 0;
+}
+
+
+// custom function x
+void x()
+{
+  // Body
+}
+
+void y()
+{
+  // Body
+}
