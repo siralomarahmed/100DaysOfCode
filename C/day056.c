d0fb1e037bc460c532b5407c3d1fb636e4f19b50
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-25 09:58:44 +0300

    Adding day056.c

---
+++ C/day056.c
@@ -0,0 +1,13 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 25 - 2020
+Description:
+This program is an example of Strings Decleration in C */
+
+
+int main()
+{
+  // String Deceleration
+  char first_sentence[40];
+
+  return 0;
+}
