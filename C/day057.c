eb53f6a6e77403b9107a9b1f4b6ab0da28e155b3
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-26 10:15:11 +0300

    Adding day057.c

---
+++ C/day057.c
@@ -0,0 +1,16 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 26 - 2020
+Description:
+This program is an example of String initialization */
+
+
+int main()
+{
+  // initialization with single quotes
+  char greeting[7]= {'H', 'e', 'l', 'l', 'o'};
+
+  // Initialization with double quotes
+  // With automatic size
+  char better_greeting[] = {"Hello World!"};
+  return 0;
+}
