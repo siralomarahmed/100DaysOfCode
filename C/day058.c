f7830b39b596e1c87176eea5b29ad358bfb735aa
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-02-27 10:17:30 +0300

    Adding day058.c

---
+++ C/day058.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: February 27 - 2020
+Description:
+This program is an example of reassigning values to a string */
+
+
+int main()
+{
+  // initilize an array with a value
+  char first_name[] = {"John"};
+
+  // Reassigning the values of the array
+  first_name[0] = 'M';
+  first_name[1] = 'i';
+  first_name[2] = 'k';
+  first_name[3] = 'e';
+  
+  return 0;
+}
