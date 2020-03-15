c55f98bba4391c42c38f247f79f742a5a6dd025c
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-03-15 12:05:03 +0300

    Adding day075.c

---
+++ C/day075.c
@@ -0,0 +1,19 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: March 15 - 2020
+Description:
+This program is an example of pointer deceleration */
+
+
+int main(void)
+{
+  // Deceleration of a pointer with variable type integer
+  int *pbox_1;
+
+  // Deceleration of a pointer with variable type Character
+  char *pbox_2;
+
+  // Deceleration of a pointer with variable type Float
+  float *pbox_3;
+
+  return 0;
+}
