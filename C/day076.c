e6c4bb17a5a17f3dcc313d6d58985845ffc95f4e
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-03-16 14:03:27 +0300

    Adding day076.c

---
+++ C/day076.c
@@ -0,0 +1,18 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: March 16 - 2020
+Description:
+This program is an example of pointer Initialization */
+
+
+#include <stddef.h>
+
+int main(void)
+{
+  // Initialization integer pointer with a NULL value
+  int *pbox_1 = NULL;
+
+  // Initialization integer pointer with a custom value
+  char *pbox_2 = 80;
+
+  return 0;
+}
