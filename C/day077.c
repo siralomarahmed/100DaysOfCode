aeb14dac5a543e3b4df828747df35b28cd79a5ce
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-03-17 12:14:50 +0300

    Adding day077.c

---
+++ C/day077.c
@@ -0,0 +1,16 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: March 17 - 2020
+Description:
+This program is an example of pointer Initialization using the address operator */
+
+
+int main(void)
+{
+  // Defining a variable
+  int box1 = 200;
+
+  // Defining a pointer with the address operator
+  int *pbox_2 = &box1;
+
+  return 0;
+}
