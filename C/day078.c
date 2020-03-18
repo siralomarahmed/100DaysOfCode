1ca839b9b8407747e455a13ff7de68ded5dc0d65
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-03-18 11:55:36 +0300

    Adding day078.c

---
+++ C/day078.c
@@ -0,0 +1,14 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: March 18 - 2020
+Description:
+This program is an example of pointer Initialization and decleration using the address operator */
+
+
+#include <stddef.h>
+
+int main(void)
+{
+  int triangle = 33, *pcircle = &triangle, rectangle = NULL, *pcube = 300;
+  
+  return 0;
+}
