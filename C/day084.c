56b753a41c6e7447815022dd7de9a5d333a1cb57
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-03-24 17:57:22 +0300

    Adding day084.c

---
+++ C/day084.c
@@ -0,0 +1,15 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: March 24 - 2020
+Description:
+This code is an example of a pointer in arrays
+In this program, the pointer will get the first element in the array by assigning the array to the pointer. */
+
+
+int main(void)
+{
+  int my_array[20] = {1, 2, 3, 4, 5};
+
+  int *my_pointer = my_array;
+
+  return 0;
+}
