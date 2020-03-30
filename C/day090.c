07e7b225f457306505aa3c9569354e4597bfa4c5
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-03-30 16:59:33 +0300

    Adding day090.c

---
+++ C/day090.c
@@ -0,0 +1,18 @@
+/* Author: Ahmed A. M. ALOMAR
+Date: March 30 - 2020
+Description:
+This code is an example of a structure decleration inside the main function */
+
+
+int main(void)
+{
+  struct numbers
+  {
+    int naturals;
+    int integers;
+    int real;
+    float rational;
+  };
+  
+  return 0;
+}
