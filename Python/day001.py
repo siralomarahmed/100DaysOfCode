1a2d78527d6bb0b4796763b99ae554c46d2b0755
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-11 18:48:57 +0300

    Adding day001.py

---
+++ Python/day001.py
@@ -0,0 +1,21 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 11 - 2020
+#
+# Description:
+# * Example of a python script file.
+# * Example of comments.
+# ------------------------------------ #
+
+
+
+# This is a comment
+
+# A comment in python Can be created on multiple lines,
+# by Adding 
+# the number sign 
+# over
+# and over
+# again.. .
+
+# This is another comment.
