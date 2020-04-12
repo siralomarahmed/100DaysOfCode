043c8622dad7b33677d7d70fdb661acc57b27127
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-12 16:21:02 +0300

    Adding day002.py source code

---
+++ SourceCode/Python/day002.py
@@ -0,0 +1,25 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 12 - 2020
+#
+# Description:
+# * Example a python script.
+# * This script will print "Hello World"
+#   To the terminal using the print
+#   function.
+# * And More examples of printing
+#   other strings
+# ------------------------------------ #
+
+
+print("Hello World")
+
+print("I am coding in python this time. ^_^/")
+print("I wonder what I will learn this time?")
+
+print("As they say, I am a python coder now!")
+print("If you wrote code in python, you are a python coder.")
+print("The beginning is always exciting, I cant wait.. . XD")
+
+print("------------------------------------------------------ boo !")
+print("   Yes, print can print any character")
