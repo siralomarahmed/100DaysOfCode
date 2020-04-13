286501bae3a6eed894b108ef6eb1801a0ac04f19
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-13 17:39:09 +0300

    Adding day003.py

---
+++ SourceCode/Python/day003.py
@@ -0,0 +1,42 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 13 - 2020
+#
+# Description:
+# * Examples of basic python data types
+# * Example of using the type() function
+# ------------------------------------ #
+
+
+# Boolean
+True
+False
+
+print(True)
+print(type(True))
+print(False)
+print(type(False))
+
+# Integer Number
+2
+
+print(2)
+print(type(2))
+
+# Float Number
+3.14
+
+print(3.14)
+print(type(3.14))
+
+# Character
+'H'
+
+print('H')
+print(type('H'))
+
+# String
+"Computer Science"
+
+print("Computer Science")
+print(type("Computer Science"))
