0146364990a1a904fa78481de9c5489950f47e2b
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-14 17:30:53 +0300

    Adding day004.py

---
+++ SourceCode/Python/day004.py
@@ -0,0 +1,65 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 14 - 2020
+#
+# Description:
+# * Example of using Arthemetic operators
+#   on basic data types
+# ------------------------------------ #
+
+
+# On Numbers
+print("On Numbers: ")
+
+## Addition
+print(1 + 1)
+
+## Subtraction
+print(2 - 1)
+
+## Multiplication
+print(3 * 2)
+
+## Exponent
+print(3 ** 4)
+
+## Division
+print(5 / 2)
+
+## Floor division
+print(5 // 2)
+
+## Modulos
+print(5 % 2)
+
+
+# On Strings
+print("On Strings: ")
+
+## Addition
+print("Ahmed" + "Alomar")
+
+## Multiplication
+print("Ahmed" * 5)
+
+
+# On Booleans
+print("On Booleans: ")
+
+print(False + False)
+print(False + 100)
+print(False - 100)
+print(False * 100)
+print(False ** 100)
+print(False / 100)
+print(False // 100)
+print(False % 100)
+print(True + False)
+print(True + True)
+print(True + 100)
+print(True - 100)
+print(True * 100)
+print(True ** 100)
+print(True / 100)
+print(True // 100)
+print(True % 100)
