b64a63436854d490a815e89373e3fc4ec0becbe4
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-15 18:01:57 +0300

    Adding day005.py

---
+++ SourceCode/Python/day005.py
@@ -0,0 +1,25 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 15 - 2020
+#
+# Description:
+# * Example of Variable Assignments
+#   with basic python data types.
+# ------------------------------------ #
+
+
+# String Variable:
+device_type = "Phone"
+print(device_type)
+
+# Floating point number Variable:
+device_price = 100.99
+print(device_price)
+
+# Integer number variable
+storage_capacity = 512
+print(storage_capacity)
+
+# Boolean variable
+water_risistant = True
+print(water_risistant)
