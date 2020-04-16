af9cb7eab1411a6c1222a5070df414cba88a67c1
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-16 17:51:16 +0300

    Adding day006.py

---
+++ SourceCode/Python/day006.py
@@ -0,0 +1,41 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 16 - 2020
+#
+# Description:
+# * Example of using the assignment
+#   operators with numbers
+# ------------------------------------ #
+
+
+# Assign
+rank = 12
+print(rank)
+
+# Add and Assign
+rank += 1
+print(rank)
+
+# Subtract and Assign
+rank -= 1
+print(rank)
+
+# Multiply and Assign
+rank *= 2
+print(rank)
+
+# Exponent and Assign
+rank **= 2
+print(rank)
+
+# Divide and Assign
+rank /= 2
+print(rank)
+
+# Floor Division and assign
+rank //= 2
+print(rank)
+
+# Modulus and Assign
+rank %= 7
+print(rank)
