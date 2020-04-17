aef988732b60e58a649c20145a297eb654010af3
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-17 17:13:44 +0300

    Adding day007.py

---
+++ SourceCode/Python/day007.py
@@ -0,0 +1,43 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 17 - 2020
+#
+# Description:
+# * Example of string variable definition
+# * Example of creating strings
+# * Example of row string
+# ------------------------------------ #
+
+
+# String variable definition
+name = "Ahmed"
+print(name)
+
+# String literal in a single quotes
+print('Programming')
+
+# String literal in a double quotes
+print("Computer Science")
+
+# Multi-line String in a single quotes
+print(''' The study of time and space costs, 
+That are associated with different approaches, 
+
+to solve a multitude of computational problems.''')
+
+# Multi-line String in a double quotes
+print("""A database 
+is intended to:  
+  * organize, 
+  * store, 
+  * and retrieve large amounts of data easily.""")
+
+
+## Escape characters example
+nice_day = 'Today\'s weather is very beautiful'
+print(nice_day)
+
+
+# Row String
+# will not add new line or a tab
+print(r"C:\news\travel")
