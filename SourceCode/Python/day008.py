cab30d63aef84197d1d84c12181bf91ec4c66f70
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-18 17:00:51 +0300

    Adding day008.c

---
+++ SourceCode/Python/day008.py
@@ -0,0 +1,18 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 18 - 2020
+#
+# Description:
+# * Example of String concatenation
+# ------------------------------------ #
+
+
+# Some variables
+first_name = "Ahmed"
+last_name = "Alomar"
+
+# String concatenation using the plus operator
+print("Hello there, " + first_name + ", your last name is: " + last_name + ".")
+
+# String concatenation using the pre-formatted string techneque
+print(f"Hello there, {first_name}, Your last name is: {last_name}.")
