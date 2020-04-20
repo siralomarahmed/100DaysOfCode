4627e27638fdccad171695821d2862bc3d64296c
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-20 16:48:36 +0300

    Adding day010.py

---
+++ SourceCode/Python/day010.py
@@ -0,0 +1,25 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 20 - 2020
+#
+# Description:
+# * Example of getting user data
+# * Example of getting the length of string
+# ------------------------------------ #
+
+
+# An introduction message
+print("Wellcome to the club.")
+
+# Some variables to get user input:
+name = input(" Who are you? ")
+age = input("How old are you? ")
+
+# Providing an output
+print(f".. .Alright, {name} you are going with the other {str(age)} years old people. Have fun.")
+
+
+print("-------------------------ok you can go. .")
+
+# String length
+print("Wait.. . let me see, your name has " + str(len(name)) + " characters.. ok, you can go now. thanks!")
