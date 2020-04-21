a9de926cb30164b84d9336a4671c712826400a2f
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-21 16:56:08 +0300

    Adding day011.py

---
+++ SourceCode/Python/day011.py
@@ -0,0 +1,28 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 21 - 2020
+#
+# Description:
+# * Example of lists definition
+# ------------------------------------ #
+
+
+# an empty list 
+empty_list = []
+
+
+# list of strings
+animals = ["Dog", "Cat"]
+
+# list of numbers
+phones = [123321, 456654, 789987]
+
+# list of multiple data types
+address = [123, "2nd street of high building", "awesome country!", 123456]
+
+# list of lists (multideminsional list)
+zoo = [
+  ["Lion", "Tiger", "Cat"],
+  ["WOlf", "Fox", "Dog"],
+  ["Monkey", "Frog", "Rabbit"]
+]
