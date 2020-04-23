297d4629977ad5b69e317c8d323c91c807164101
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-23 18:59:32 +0300

    Adding day013.py

---
+++ SourceCode/Python/day013.py
@@ -0,0 +1,29 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 23 - 2020
+#
+# Description:
+# * Example of Tuple defenitions
+# ------------------------------------ #
+
+
+# Define an empty tuple
+empty = ()
+
+
+# Tuple Definition with data
+coder = "Write the source code", "Use algorithems", "Build the solution"
+print(coder)
+
+# Tuple Definition with parenthisis
+programmer = ("optimize the source code", "Design algorthems", "Plan solutions")
+print(programmer)
+
+
+# Multiple data type tuple
+multi_things = 123, "Hello", True, ["World", "Universe"]
+print(multi_things)
+
+
+# Index reference
+print(programmer[1])
