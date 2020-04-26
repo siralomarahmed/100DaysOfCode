8d075e833fee33f0535cfdfa2ef4046535015007
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-26 11:36:10 +0300

    Adding day016.py

---
+++ SourceCode/Python/day016.py
@@ -0,0 +1,21 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 26 - 2020
+#
+# Description:
+# * Example of function definitions
+# ------------------------------------ #
+
+
+# define an empty function
+def empty_function():
+  pass
+
+# A function that prints multiple greeting
+def greeting():
+  print("Hello.. .")
+  print("Wellcome to the party")
+
+
+# printing the function call
+print(greeting())
