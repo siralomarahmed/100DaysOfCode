06bcc023442c48405359d0609dda3d5c717d418b
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-27 15:26:38 +0300

    Adding day017.py

---
+++ SourceCode/Python/day017.py
@@ -0,0 +1,20 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 27 - 2020
+#
+# Description:
+# * Example of function parameter
+#   and argument
+# * An example of a program that will
+#   print a greeting provided a name.
+# ------------------------------------ #
+
+
+# function definition with one parameter
+def greetings(name):
+  print(f"Hello {name}, wellcome to the party!")
+  print("We hope you are going to have fun")
+
+
+# Function call with argument
+greetings("Ahmed")
