c5965d7d23aab7c63264803157f9d7ba5f785906
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-04-29 13:05:11 +0300

    Adding day019.py

---
+++ SourceCode/Python/day019.py
@@ -0,0 +1,23 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: April 29 - 2020
+#
+# Description:
+# * Example of using methods on strings
+# ------------------------------------ #
+
+
+# A string
+some_say = "This argument is formally valid"
+
+# .lower() .upper() .title()
+print(some_say.lower())
+print(some_say.upper())
+print(some_say.title())
+
+
+# Strip()
+so_much_space = "           THere was once space with me! Now I don\'t know what happened!               "
+
+print(so_much_space)
+print(so_much_space.strip())
