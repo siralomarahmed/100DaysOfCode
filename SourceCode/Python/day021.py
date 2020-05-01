71e00c2c7ebd0c5395924623fef9e2d2f15aea5e
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-01 13:59:48 +0300

    Adding day021.py

---
+++ SourceCode/Python/day021.py
@@ -0,0 +1,54 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 1 - 2020
+#
+# Description:
+# * Example of using comparison & logical
+#   operators
+# ------------------------------------ #
+
+
+# Comparison Operators:
+## Equal
+print(2 == 2)
+print(2 == 1)
+
+## Not Equal
+print(2 != 2)
+print(2 != 1)
+
+## Greater than
+print(3 > 2)
+print(3 > 4)
+print(3 > 3)
+
+## Greater than or equal
+print(3 >= 2)
+print(3 >= 4)
+print(3 >= 3)
+
+## Less than
+print(3 < 2)
+print(3 < 4)
+print(3 < 3)
+
+## Less than or equal
+print(3 <= 2)
+print(3 <= 4)
+print(3 <= 3)
+
+
+# Logical operators:
+## AND
+print(True and True)
+print(True and False)
+print(False and False)
+
+## OR
+print(True or True)
+print(True or False)
+print(False or False)
+
+## NOT
+print(not True)
+print(not False)
