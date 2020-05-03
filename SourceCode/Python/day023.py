e8b6132d4a160d11c63044e0af052c471ca89bfc
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-03 13:49:08 +0300

    Adding day023.py

---
+++ SourceCode/Python/day023.py
@@ -0,0 +1,25 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 3 - 2020
+#
+# Description:
+# * Example of Dictionary Definition
+# ------------------------------------ #
+
+
+# Dictionary Decleration
+languages = {}
+
+
+# Dictionary Initialization
+languages[1] = "Ruby"
+languages[2] = "JavaScript"
+languages[3] = "Python"
+
+
+# Dictionary Definition
+activity = {
+  "software development": ["desktop", "mobile", "web"],
+  "web development": ["front end", "back end", "full stack"],
+  "front end": ["structure", "design", "accessability"]
+}
