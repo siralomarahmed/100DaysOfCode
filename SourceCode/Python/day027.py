c963b425d198976136b3932e5b47413ba46aa3e4
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-07 16:34:50 +0300

    Adding day027.py

---
+++ SourceCode/Python/day027.py
@@ -0,0 +1,19 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 7 - 2020
+#
+# Description:
+# * Example of If Statement
+#   THis is a function that will take
+#   a number and return if it is positive
+#   or negative
+# ------------------------------------ #
+
+
+def positive_or_negative(number):
+  if number > 0:
+    return "Positive!"
+  elif number < 0:
+    return "Negative!"
+  else:
+    return "It is ZERO!"
