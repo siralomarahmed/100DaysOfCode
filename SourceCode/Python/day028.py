f875ed2dd1f2481bb620e7a1ed4d80b825548969
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-08 14:05:43 +0300

    Adding day028.py

---
+++ SourceCode/Python/day028.py
@@ -0,0 +1,13 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 8 - 2020
+#
+# Description:
+# * Example of while loop
+# ------------------------------------ #
+
+count = 0
+while count <= 10:
+  print(count)
+  count += 1
+
