e81b05b1714e8eb9b43b43001fc884f3ce31220d
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-09 17:45:44 +0300

    Adding day029.py

---
+++ SourceCode/Python/day029.py
@@ -0,0 +1,17 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 9 - 2020
+#
+# Description:
+# * Example of conditional expression
+# * This program will check if the mark
+#   is above 75 or below it, and will
+#   calculate if it is a pass or faild
+#   Mark
+# ------------------------------------ #
+
+
+mark = 80
+
+test_results = "Pass" if mark >= 75 else "Faild"
+print(test_results)
