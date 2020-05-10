6156298f5ce9b593ba387e0a6d3743c4451ff29c
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-10 14:16:38 +0300

    Adding day030.py

---
+++ SourceCode/Python/day030.py
@@ -0,0 +1,15 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 10 - 2020
+#
+# Description:
+# * Example of a for loop
+# * this loop will print every element
+#   inside the list
+# ------------------------------------ #
+
+
+members = ["Ahmed", "Bob", "Dan", "Cathy", "Eddy"]
+
+for x in members:
+  print(x)
