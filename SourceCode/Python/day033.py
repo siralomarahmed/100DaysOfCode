3656fe41be36ca05a2c21db089cc0347759c9a9c
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-13 17:41:06 +0300

    Adding day033.py

---
+++ SourceCode/Python/day033.py
@@ -0,0 +1,13 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 13 - 2020
+#
+# Description:
+# * Example of Enumeration
+# ------------------------------------ #
+
+
+pokemon_list = ["Pikachu", "Eevee", "Snorlax", "Charizard", "Celebi", "Ditto"]
+
+for index, pokemon in enumerate(pokemon_list):
+  print(index, pokemon)
