a681d82ea203beb7de1c2ba560e793daf3796d07
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-19 13:11:22 +0300

    Adding day039.py

---
+++ SourceCode/Python/day039.py
@@ -0,0 +1,25 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 19 - 2020
+#
+# Description:
+# * Example of Using standard module
+#   library. math, string
+# ------------------------------------ #
+
+
+# importing a module
+import math
+
+# Using the math module attributes
+
+print(math.floor(3.77))
+print(math.sqrt(753))
+print(math.pi)
+
+
+# importing the string module
+import string
+
+# Using the string module
+print(string.capwords("ahmed a. m. alomar"))
