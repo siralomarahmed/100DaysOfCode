851e5574c37aeaa92e213298da1daaf8cdf54248
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-21 11:59:25 +0300

    Adding day041 progress

---
+++ SourceCode/Python/day041.py
@@ -0,0 +1,16 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 21 - 2020
+#
+# Description:
+# * Example of Module Aliasing
+# ------------------------------------ #
+
+
+# giving the math module a name of m
+import math as m
+
+
+print(m.pi)
+print(m.floor(3.3829))
+print(m.fabs(-45))
