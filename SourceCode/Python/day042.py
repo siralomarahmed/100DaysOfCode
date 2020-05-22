ac99a25bbd9df56131c4519acf34e2940bf09a01
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-22 12:26:58 +0300

    Adding day042 code

---
+++ SourceCode/Python/day042.py
@@ -0,0 +1,16 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 22 - 2020
+#
+# Description:
+# * Example of Class Definition
+# ------------------------------------ #
+
+
+# Example of BigBox class
+class BigBox():
+  pass
+
+# Example of a SmallBox class
+class SmallBox():
+  pass
