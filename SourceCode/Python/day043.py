fd1445da84f2041828b6b28fdc08b19690fdeac6
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-23 11:42:51 +0300

    adding day043 code

---
+++ SourceCode/Python/day043.py
@@ -0,0 +1,24 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 23 - 2020
+#
+# Description:
+# * Example of Class Instantiation
+# * In this program, there will be a
+#   class names and an example of creating
+#   instance of it. these instance are
+#   object of the class.
+# ------------------------------------ #
+
+
+# Class definition
+class Names():
+  pass
+
+# class instantiation
+ahmed = Names()
+bob = Names()
+
+
+print(ahmed)
+print(bob)
