7109af91fd5b8ed04c172bf5e61f82a505e71f25
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-24 12:17:00 +0300

    Adding day044 code

---
+++ SourceCode/Python/day044.py
@@ -0,0 +1,20 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 24 - 2020
+#
+# Description:
+# * Example of Class Initialization
+# ------------------------------------ #
+
+
+class Animals():
+# Class initialization
+  def __init__(self):
+    print("Hello, I am very cute!")
+
+
+cat = Animals()
+dog = Animals()
+
+print(cat)
+print(dog)
