0f90e075f7ae93fb9d52ae7c1fe053f4fdf1c36d
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-05-29 14:14:18 +0300

    Adding day049 code

---
+++ SourceCode/Python/day049.py
@@ -0,0 +1,15 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: May 29 - 2020
+#
+# Description:
+# * Example of Function Documentation
+# ------------------------------------ #
+
+
+def my_awesomeness(name):
+  """This function will take a string
+  and return a string with greetings
+  that is it, that is all what the function will do!"""
+
+help(my_awesomeness)
