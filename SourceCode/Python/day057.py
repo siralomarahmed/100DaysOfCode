c4bb9d59d2975dcf830d7ad70c7d02e4eefc8ef8
Ahmed A. M. ALOMAR <ahmedamalomar@hotmail.com>
2020-06-06 16:14:33 +0300

    Adding day 057.py code

---
+++ SourceCode/Python/day057.py
@@ -0,0 +1,17 @@
+# ------------------------------------ #
+# Author: Ahmed A. M. ALOMAR
+# Date: June 6 - 2020
+#
+# Description:
+# * Example of While Loop
+# * Example of a while loop that will
+#   print out the numbers from 0 to 6
+# ------------------------------------ #
+
+
+count = 0           # a variable to be used as a counting thing
+while count <= 5:   # the loop difinition with the condition
+  print(count)
+  count += 1        # the condition to not make an infinite loop
+
+print(count)        # prints 6 because it was incremented after the loop
